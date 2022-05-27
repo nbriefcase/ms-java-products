0800dd56cea54d204a5838c846636ff9ceca2973
Neiro <gpbriefcase@gmail.com>
2022-05-27 11:16:20 -0500

    Gateway implemented

--- items/src/main/java/com/personal/eureka/items/client/ProductRestClient.java
+++ items/src/main/java/com/personal/eureka/items/client/ProductRestClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "product-service", url = "localhost:8001")
+@FeignClient(name = "product-service")
 public interface ProductRestClient {
 
     @GetMapping("/products")
