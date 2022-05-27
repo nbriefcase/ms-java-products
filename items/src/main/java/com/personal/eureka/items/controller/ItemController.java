07fd795ceae88bc20c489ec04cab7d95984347d2
Neiro <gpbriefcase@gmail.com>
2022-05-27 17:40:21 -0500

    Resilience4j implemented

--- items/src/main/java/com/personal/eureka/items/controller/ItemController.java
+++ items/src/main/java/com/personal/eureka/items/controller/ItemController.java
@@ -1,8 +1,10 @@
 package com.personal.eureka.items.controller;
 
 import com.personal.eureka.items.models.Item;
+import com.personal.eureka.items.models.Product;
 import com.personal.eureka.items.service.IItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -17,6 +19,9 @@ public class ItemController {
     @Autowired
     private IItemService itemService;
 
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
     @GetMapping(value = "/items")
     public List<Item> list(@RequestHeader(name = "token-request") String token, @RequestParam(name = "name") String name) {
         System.out.println("token:" + token);
@@ -26,6 +31,15 @@ public class ItemController {
 
     @GetMapping(value = "/item/{id}/amount/{amount}")
     public Item get(@PathVariable Long id, @PathVariable Integer amount) {
-        return itemService.findById(id, amount);
+        return circuitBreakerFactory.create("items")
+                .run(() -> itemService.findById(id, amount), e -> defaultItem(id, amount));
+    }
+
+    private Item defaultItem(Long id, Integer amount) {
+        Product product = new Product();
+        product.setProductId(id);
+        product.setDescription("Default Product");
+        product.setPrice(0.0);
+        return new Item(product, amount);
     }
 }
