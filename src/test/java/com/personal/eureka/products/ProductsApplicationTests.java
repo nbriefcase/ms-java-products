fff8fffef0677f5d7c454c34e432a9ea87d981c4
Neiro <gpbriefcase@gmail.com>
2022-05-26 21:05:03 -0500

    First Commit

---
+++ src/test/java/com/personal/eureka/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.eureka.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
