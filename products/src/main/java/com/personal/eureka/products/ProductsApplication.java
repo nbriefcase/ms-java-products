b024eef4c8c1382d0402b35d6030b1602644169b
Neiro <gpbriefcase@gmail.com>
2022-05-27 10:10:12 -0500

    Eureka clients implemented

--- products/src/main/java/com/personal/eureka/products/ProductsApplication.java
+++ products/src/main/java/com/personal/eureka/products/ProductsApplication.java
@@ -2,12 +2,14 @@ package com.personal.eureka.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsApplication.class, args);
+    }
 
 }
