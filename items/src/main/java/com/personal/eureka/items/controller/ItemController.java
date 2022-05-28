fe92d8dcd4d3c8ded6bea1d74e61c27ed1132e18
Neiro <gpbriefcase@gmail.com>
2022-05-27 22:30:36 -0500

    Simulating failures and latencies: Using @TimeLimiter with Properties file

--- items/src/main/java/com/personal/eureka/items/controller/ItemController.java
+++ items/src/main/java/com/personal/eureka/items/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.personal.eureka.items.models.Item;
 import com.personal.eureka.items.models.Product;
 import com.personal.eureka.items.service.IItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @RestController
@@ -44,6 +46,19 @@ public class ItemController {
         return itemService.findById(id, amount);
     }
 
+    @TimeLimiter(name = "items", fallbackMethod = "defaultItemTl")
+    @GetMapping(value = "/item/tl/{id}/amount/{amount}")
+    public CompletableFuture<Item> getTL(@PathVariable Long id, @PathVariable Integer amount) {
+        return CompletableFuture.supplyAsync(() -> itemService.findById(id, amount));
+    }
+
+    @TimeLimiter(name = "items") // fallbackMethod has a workaround, it should use only one when it's combined
+    @CircuitBreaker(name = "items", fallbackMethod = "defaultItem")
+    @GetMapping(value = "/item/cb/tl/{id}/amount/{amount}")
+    public CompletableFuture<Item> getCbTL(@PathVariable Long id, @PathVariable Integer amount) {
+        return CompletableFuture.supplyAsync(() -> itemService.findById(id, amount));
+    }
+
     private Item defaultItem(Long id, Integer amount, Throwable ex) {
         log.error(ex.getMessage());
         Product product = new Product();
@@ -51,5 +66,14 @@ public class ItemController {
         product.setDescription("Default Product");
         product.setPrice(0.0);
         return new Item(product, amount);
+    }
+
+    private CompletableFuture<Item> defaultItemTl(Long id, Integer amount, Throwable ex) {
+        log.error(ex.getMessage());
+        Product product = new Product();
+        product.setProductId(id);
+        product.setDescription("Default Product 2");
+        product.setPrice(0.0);
+        return CompletableFuture.supplyAsync(() -> new Item(product, amount));
     }
 }
