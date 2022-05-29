4ca802aee4ebe5d39dafdf33b8867564be6cc9c8
Neiro <gpbriefcase@gmail.com>
2022-05-29 14:02:25 -0500

    User service: Exposing repository methods

--- users/src/main/java/com/personal/eureka/users/models/repository/UserRepository.java
+++ users/src/main/java/com/personal/eureka/users/models/repository/UserRepository.java
@@ -3,12 +3,15 @@ package com.personal.eureka.users.models.repository;
 import com.personal.eureka.users.models.entity.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
-    User findByUsername(String username);
+    @RestResource(path = "username")
+    User findByUsername(@Param("name") String username);
 
     @Query("select u from User u where u.username = ?1")
     User customFindUserByUsername(String username);
