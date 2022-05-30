4804fe57714fe7570a804d2fcd0f01ed3838d56e
Neiro <gpbriefcase@gmail.com>
2022-05-30 12:56:25 -0500

    Gateway service: implemented for OAuth2 service.

--- users/src/main/resources/import.sql
+++ users/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `users` (username, password, is_active, email) VALUES ('user1','12345', 1,'user1@personal.com');
-INSERT INTO `users` (username, password, is_active, email) VALUES ('admin1','12345', 1, 'admin1@personal.com');
+INSERT INTO `users` (username, password, is_active, email) VALUES ('user1','$2a$10$81Szl60E17xzyQ16yseQT.Urm4EgKvO9CCH3inHYx/hN/3N.d1OpW', 1,'user1@personal.com');
+INSERT INTO `users` (username, password, is_active, email) VALUES ('admin1','$2a$10$dQj15j7dxfj5KEZ1Vhbm1eqxkto3R.84LrcfP/qwysvGXDaSWakQe', 1, 'admin1@personal.com');
 
 INSERT INTO `roles` (name) VALUES ('ROLE_USER');
 INSERT INTO `roles` (name) VALUES ('ROLE_ADMIN');
