0162217a580a7e643ba01cbcb60cb632be731feb
Neiro <gpbriefcase@gmail.com>
2022-05-30 19:00:37 -0500

    Fixed: Item service tests and pom.xml, CommonsUsers pom.xml

--- items/src/test/java/com/personal/eureka/items/ItemsApplicationTest.java
+++ items/src/test/java/com/personal/eureka/items/ItemsApplicationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ItemServiceApplicationTests {
+public class ItemsApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
