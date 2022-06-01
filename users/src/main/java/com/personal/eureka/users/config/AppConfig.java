ee7381f8d7976b1a50b1157365f034b78b90e040
Neiro <gpbriefcase@gmail.com>
2022-05-31 21:32:31 -0500

    RabbitMQ Bean implemented for microservices.

--- users/src/main/java/com/personal/eureka/users/config/AppConfig.java
+++ users/src/main/java/com/personal/eureka/users/config/AppConfig.java
@@ -1,17 +1,33 @@
 package com.personal.eureka.users.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class AppConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public HikariDataSource dataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+        return DataSourceBuilder.create().type(com.zaxxer.hikari.HikariDataSource.class).build();
+    }
+
+    @Bean
+    public CachingConnectionFactory defaultConnectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses(env.getProperty("ms.rabbitmq.host", "localhost"));
+        connectionFactory.setPort(Integer.parseInt(env.getProperty("ms.rabbitmq.port", "5672")));
+        connectionFactory.setUsername(env.getProperty("ms.rabbitmq.user", "user"));
+        connectionFactory.setPassword(env.getProperty("ms.rabbitmq.password", "12345"));
+        return connectionFactory;
     }
 }
